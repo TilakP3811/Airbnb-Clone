068ffdfa3dbd381ddc0d7b306f3c1606e339cab3
Tilak <tilak@webkorps.com>
2022-11-22 20:25:03 +0530

    starting of user login and sign up api

---
+++ spec/requests/api/users_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "Api::Users", type: :request do
+  describe "GET show" do
+    context "user exist" do
+      it "is successful" do
+        
+      end
+    end
+
+    context "user does not exist" do
+      it "is not found" do
+        
+      end
+    end
+  end
+end
